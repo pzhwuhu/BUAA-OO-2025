312095296ee18209f72e74f0bde6d82a2ad3416d
pzhwuhu <23373390@buaa.edu.cn>
2025-04-05 01:50:13 +0800

    fix bug in receive order

--- src/DispatchThread.java
+++ src/DispatchThread.java
@@ -44,8 +44,8 @@ public class DispatchThread extends Thread {
 
     public void personDispatch(PersonRequest request) {
         int elevatorId = inOrder();
-        subRequestMap.get(elevatorId).push(request);
         TimableOutput.println("RECEIVE-" + request.getPersonId() + "-" + elevatorId);
+        subRequestMap.get(elevatorId).push(request);
         //+ "-" + subRequestMap.get(elevatorId).getFree()
     }
 
@@ -66,7 +66,7 @@ public class DispatchThread extends Thread {
             }
             synchronized (requests) {
                 while (!requests.getFree()) {
-                    //TimableOutput.println("elevator-" + counter + "-is waiting-" + requests.getFree());
+                    //TimableOutput.println("elevator" + counter + "is wait-" + requests.getFree());
                     try {
                         requests.wait();
                     } catch (InterruptedException e) {
